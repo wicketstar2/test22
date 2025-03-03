7a17acbfb193dc0779429c9a93ba07f3c0bcd2ce
wicketstar2 <homemadesteam52@gmail.com>
2025-03-04 08:51:58 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ window.onload = function () {
 // Function to show results
 function myFunction() {
     var results = document.getElementById("results");
-    if (results) results.style.display = "block";
+    if (results) results.style.visibility ="hidden";
 }
 
 // Dummy hideButton function to prevent errors
@@ -56,17 +56,19 @@ function hideButton() {
     if (button) button.style.visibility = "hidden";
 }
 
-// gender
+// gender boy
 if (age0) {
     age0.onclick = function () {
         if (age28) age28.style.visibility ="visible";
+        if (age27) age27.style.visibility ="hidden";
     };
 }
 
-// gender
+// gender girl
 if (age01) {
     age01.onclick = function () {
         if (age27) age27.style.visibility ="visible";
+        if (age28) age28.style.visibility ="hidden";
     };
 }
 
@@ -81,7 +83,37 @@ if (age05) {
         if (age01) age01.style.visibility = "hidden";
         if (age0) age0.style.visibility = "hidden";
         if (age03) age03.style.visibility = "hidden";
+        if (age27) age27.style.visibility ="hidden";
+        if (age28) age28.style.visibility ="hidden";
     };
 }
 
+// 20s
+if (age04) {
+    age04.onclick = function () {
+        if (text1) text1.style.visibility = "visible";
+        if (results) results.style.visibility ="visible";
+        if (age05) age05.style.visibility = "hidden";
+        if (age04) age04.style.visibility = "hidden";
+        if (age01) age01.style.visibility = "hidden";
+        if (age0) age0.style.visibility = "hidden";
+        if (age03) age03.style.visibility = "hidden";
+        if (age27) age27.style.visibility ="hidden";
+        if (age28) age28.style.visibility ="hidden";
+    };
+}
 
+// 6b
+if (age1) {
+    age1.onclick = function () {
+        if (text1) text1.style.visibility = "visible";
+        if (results) results.style.visibility ="visible";
+        if (age05) age05.style.visibility = "hidden";
+        if (age04) age04.style.visibility = "hidden";
+        if (age01) age01.style.visibility = "hidden";
+        if (age0) age0.style.visibility = "hidden";
+        if (age03) age03.style.visibility = "hidden";
+        if (age27) age27.style.visibility ="hidden";
+        if (age28) age28.style.visibility ="hidden";
+    };
+}
